ff87399b9d5f98f65fefad7bcc11736ab49c1687
sandyasekar <sandya.sekar@ui.ac.id>
2017-09-22 00:21:31 +0700

    APAP tutorial 3

---
+++ tutorial03/src/main/java/com/example/demo/tutorial3/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.example.demo.tutorial3.service;
+import java.util.List;
+import com.example.demo.tutorial3.model.StudentModel;
+
+public interface StudentService {
+	StudentModel selectStudent(String npm);
+	
+	List<StudentModel> selectAllStudents();
+	
+	void addStudent(StudentModel student);
+}
